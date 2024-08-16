2f049bec1f66ee7a84b1a8c921b9f49282bff1b0
huipadyam <cekim3197@ewhain.net>
2024-08-17 07:06:39 +0900

    #3 remove : delete 비밀번호 불일치 오류(401) 처리 on asyncHandler.js(이미 각 라우터 처리함)

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -17,13 +17,13 @@ function asyncHandler(handler) {
       ) {
         // 리소스를 찾을 수 없는 경우 처리 (404)
         res.status(404).send({ message: '리소스를 찾을 수 없습니다.' });
-      } else if (
+      } /* else if (
         e.message === 'PostPasswordMismatch' || 
         e.message === 'GroupPasswordMismatch'
       ) {
         // 비밀번호 불일치 오류 처리 (401)
         res.status(401).send({ message: '비밀번호가 틀렸습니다.' });
-      } else {
+      } */ else {
         // 기타 모든 오류 처리
         res.status(500).send({ message: '서버 오류가 발생했습니다.' });
       }
